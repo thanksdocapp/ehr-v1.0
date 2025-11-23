fbd76ca89664ddad99579a42d165f3e614e8bd36
thanksdocapp <thanksdocapp@gmail.com>
2025-11-23 18:42:17 +0000

    Fix: Remove SoftDeletes from EmailLog model
    - Removed SoftDeletes trait from EmailLog model as email_logs table doesn't have deleted_at column
    - This fixes SQL error: Column not found: 1054 Unknown column 'email_logs.deleted_at'

--- app/Models/EmailLog.php
+++ app/Models/EmailLog.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EmailLog extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
@@ -46,8 +45,7 @@ class EmailLog extends Model
         'sent_at' => 'datetime',
         'opened_at' => 'datetime',
         'created_at' => 'datetime',
-        'updated_at' => 'datetime',
-        'deleted_at' => 'datetime'
+        'updated_at' => 'datetime'
     ];
 
     /**
