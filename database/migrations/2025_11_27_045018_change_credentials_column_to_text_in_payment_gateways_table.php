5d663fd7aaa908826477319763c569d08e28b79d
thanksdocapp <thanksdocapp@gmail.com>
2025-11-27 04:50:42 +0000

    Fix payment gateway credentials constraint: change column from json to text - Change credentials column from json to text type - Allows encrypted strings to be stored properly - Fixes SQLSTATE[23000] constraint violation error

---
+++ database/migrations/2025_11_27_045018_change_credentials_column_to_text_in_payment_gateways_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('payment_gateways', function (Blueprint $table) {
+            // Change credentials column from json to text to allow encrypted strings
+            $table->text('credentials')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('payment_gateways', function (Blueprint $table) {
+            // Revert back to json (note: this may fail if data is encrypted)
+            $table->json('credentials')->change();
+        });
+    }
+};
