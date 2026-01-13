27ff424413857638ff54768ef980ab8c2b52546a
thanksdocapp <thanksdocapp@gmail.com>
2026-01-13 00:37:22 +0000

    Update form submission handler comments

--- resources/views/staff/medical-records/show.blade.php
+++ resources/views/staff/medical-records/show.blade.php
@@ -1008,7 +1008,7 @@ $(document).ready(function() {
     // Initialize remove buttons
     updateRemoveButtons();
 
-    // Simple form submission - validate files before submit
+    // Simple form submission - just validate files before submit
     $('#addAttachmentForm').on('submit', function(e) {
         // Check if files are selected
         const fileInputs = $(this).find('input[type="file"]');
@@ -1023,12 +1023,10 @@ $(document).ready(function() {
         if (!hasFiles) {
             e.preventDefault();
             alert('Please select at least one file to upload.');
-            return false; // Explicitly prevent submission
+            return false;
         }
         
-        // Files are selected - allow form to submit normally
-        // Don't call preventDefault() - form will submit naturally
-        return true; // Explicitly allow submission
+        // Let form submit normally - no preventDefault
     });
 
     // Reset form when modal is closed
